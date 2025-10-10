1cb6b1f6c63172a5bac7af90330f3a786d6e7b84
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-10-10 15:55:46 -0300

    localStorage com array
    melhorei o salvamento das informações no localStorage usando array e adicionei o campo para conteúdo das tarefas

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const inputTitle = document.getElementById('title')  // input do título da tarefa
-const saveTask = document.querySelector('#saveTask')  //botão de salvar tarefa
+const inputTitle = document.getElementById('title')       //input do título
+const inputContent = document.getElementById('content')   //input do conteúdo
+const saveTask = document.querySelector('#saveTask')      //botão de salvar
 
+saveTask.addEventListener('click', () => {   //evento click
+    const title = inputTitle.value.trim()
+    const content = inputContent.value.trim()
 
-saveTask.addEventListener('click', ()=>{             //função
-    const title = inputTitle.value                   //valor do título
-    //console.log(title)
-    localStorage.setItem(`task: ${title}`, title)    //salva o valor 
-    updateSavedTasksLis(title)                      //mostra na lista lateral
+    if (title === '' || content === '') {
+        alert('Preencha título e conteúdo da tarefa.')       //garante que tenha um conteúdo sempre
+        return
+    }
+ 
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [] //recupera array ou cria um novo
+    tasks.push({ title, content })                         
+    localStorage.setItem('tasks', JSON.stringify(tasks))  //salva array no localstorage
+
+    updateSavedTasksList()                             //atualiza a lista lateral
+
+    inputTitle.value = ''                             //limpa os campos
+    inputContent.value = ''
 })
 
-function updateSavedTasksLis(title) {
+//atualiza a lista lateral com os títulos salvos
+function updateSavedTasksList() {
     const taskSaved = document.querySelector('.list-tasks-saved')
-    const p = document.createElement('p')  // cria um novo elemento <p>
-    p.classList.add(title)
-    p.textContent = title                  // define o texto do <p> como o título da tarefa
-    taskSaved.appendChild(p)              // adiciona o <p> à lista lateral
+    taskSaved.innerHTML = ''                                      //limpa a lista antes de atualizar
+
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+    tasks.forEach(task => {
+    const p = document.createElement('p')
+    p.setAttribute('class', 'saved')
+    p.textContent = task.title              //mostra o título da tarefa
+    taskSaved.appendChild(p)
+  })
 }
+
+//carrega a lista ao abrir a página
+window.addEventListener('load', updateSavedTasksList)
